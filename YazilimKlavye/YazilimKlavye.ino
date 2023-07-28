a75cd9af2d2e1b996f163c07590789a18a965909
Semih AZ <semih.az@hotmail.com.tr>
2023-07-28 22:44:52 +0300

    v1

---
+++ YazilimKlavye/YazilimKlavye.ino
@@ -0,0 +1,129 @@
+#include <Keypad.h>
+#include "TRKeyboard.h"
+
+const byte Satir = 4;  // keypad satır sayısı
+const byte Sutun = 4;  // keypad sütün sayısı
+String Sifre;          // girilen şifre
+
+// Keypad tuş yapısını hazırlıyoruz.
+char key[Satir][Sutun] = {
+  { '1', '2', '3', 'A' },
+  { '4', '5', '6', 'B' },
+  { '7', '8', '9', 'C' },
+  { '*', '0', '#', 'D' }
+};
+
+// Arduino bağlantı bacaklarını belirliyoruz
+byte SatirPinleri[Satir] = { 2, 3, 4, 5 };
+byte SutunPinleri[Sutun] = { 6, 7, 8, 9 };
+
+// Keypade TusTakimi ismini verip arduino bağlantılarını ve karakterleri
+// bu TusTakimi keypad nesnesine tanımlıyoruz.
+Keypad TusTakimi = Keypad(makeKeymap(key), SatirPinleri, SutunPinleri, Satir, Sutun);
+
+void setup() {
+  Serial.begin(9600);
+  TRKeyboard.begin();
+}
+
+void loop() {
+  char basilanTus = TusTakimi.getKey();  // Keyped basılan tuş değerini alıyoruz.
+
+  switch (basilanTus) {
+    case '1':  // İf
+      TRKeyboard.print("if");
+      TRKeyboard.press(KEY_TAB);
+      TRKeyboard.releaseAll();
+      TRKeyboard.press(KEY_TAB);
+      TRKeyboard.releaseAll();
+      break;
+    case '2':  // Else
+      TRKeyboard.print("e");
+      TRKeyboard.press(KEY_ESC);
+      delay(500);
+      TRKeyboard.print("lse");
+      TRKeyboard.press(KEY_RIGHT_ALT);
+      TRKeyboard.press('7');
+      TRKeyboard.releaseAll();
+      TRKeyboard.press(KEY_RETURN);
+      TRKeyboard.print("/* Yorum*/");
+      delay(1000);
+      TRKeyboard.press(KEY_RETURN);
+      TRKeyboard.press(KEY_RIGHT_ALT);
+      TRKeyboard.press('0');
+      TRKeyboard.releaseAll();
+      TRKeyboard.press(KEY_RETURN);
+      TRKeyboard.releaseAll();
+      break;
+    case '3':  // İf Else
+      TRKeyboard.print("i");
+      TRKeyboard.press(KEY_ESC);
+      delay(500);
+      TRKeyboard.print("f (true)");
+      TRKeyboard.press(KEY_RIGHT_ALT);
+      TRKeyboard.press('7');
+      TRKeyboard.releaseAll();
+      TRKeyboard.press(KEY_RETURN);
+      TRKeyboard.print("/* Yorum*/");
+      delay(1000);
+      TRKeyboard.press(KEY_RETURN);
+      TRKeyboard.press(KEY_RIGHT_ALT);
+      TRKeyboard.press('0');
+      TRKeyboard.releaseAll();
+      TRKeyboard.print("e");
+      TRKeyboard.press(KEY_ESC);
+      delay(500);
+      TRKeyboard.print("lse");
+      TRKeyboard.press(KEY_RIGHT_ALT);
+      TRKeyboard.press('7');
+      TRKeyboard.releaseAll();
+      TRKeyboard.press(KEY_RETURN);
+      TRKeyboard.print("/* Yorum*/");
+      delay(1000);
+      TRKeyboard.press(KEY_RETURN);
+      TRKeyboard.press(KEY_RIGHT_ALT);
+      TRKeyboard.press('0');
+      TRKeyboard.releaseAll();
+      TRKeyboard.press(KEY_RETURN);
+      TRKeyboard.releaseAll();
+      break;
+    case '4':  // for
+      TRKeyboard.print("for");
+      TRKeyboard.press(KEY_TAB);
+      TRKeyboard.releaseAll();
+      TRKeyboard.press(KEY_TAB);
+      TRKeyboard.releaseAll();
+      break;
+
+    case '5':
+      TRKeyboard.print("switch");
+      TRKeyboard.press(KEY_TAB);
+      TRKeyboard.releaseAll();
+      TRKeyboard.press(KEY_TAB);
+      TRKeyboard.releaseAll();
+      break;
+
+
+    case 'A':  // VS Kod Düzenleme
+      TRKeyboard.press(KEY_LEFT_CTRL);
+      TRKeyboard.press('k');
+      TRKeyboard.press('d');
+      delay(500);
+      TRKeyboard.releaseAll();
+      break;
+
+
+
+
+
+
+
+    case '0':
+      TRKeyboard.press(KEY_LEFT_ALT);
+      TRKeyboard.press(KEY_LEFT_SHIFT);
+      TRKeyboard.press('F');
+      delay(500);
+      TRKeyboard.releaseAll();
+      break;
+  }
+}
